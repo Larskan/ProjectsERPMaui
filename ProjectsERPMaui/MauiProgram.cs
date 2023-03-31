7fae4f18a52c1df4332ff15d7f9bc562b5392e9d
J_H_Christensen <janx802y@easv365.dk>
2023-03-31 13:49:43 +0200

    update Pomo Page addet (only base look)

--- ProjectsERPMaui/MauiProgram.cs
+++ ProjectsERPMaui/MauiProgram.cs
@@ -19,11 +19,13 @@ public static class MauiProgram
         builder.Services.AddSingleton<StartViewModel>();
         builder.Services.AddSingleton<ProjectViewModel>();
         builder.Services.AddSingleton<TaskViewModel>();
+		builder.Services.AddSingleton<PomodoroViewModel>();
 
 		builder.Services.AddTransient<LoginView>();
         builder.Services.AddTransient<ProjectView>();
         builder.Services.AddTransient<StartView>();
 		builder.Services.AddTransient<TaskView>();
+		builder.Services.AddTransient<PomodoroView>();
 
         return builder.Build();
 	}
---
+++ ProjectsERPMaui/ViewModel/PomodoroViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ProjectsERPMaui.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsERPMaui.ViewModel
+{
+    [QueryProperty(nameof(ProjTask),nameof(ProjTask))]
+    public partial class PomodoroViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public ProjectTask _projTask;
+        public PomodoroViewModel() 
+        {
+            ProjTask = new ProjectTask();
+        }
+    }
+}
